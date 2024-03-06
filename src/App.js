a132cd108a57ab60424f39d65f63739438c550e1
sergenadin <150278010+sergenadin@users.noreply.github.com>
2024-03-06 13:35:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Map from './Map';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Map />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Map.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+};
+
+export default Map;
