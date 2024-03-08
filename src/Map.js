1e0627b1cdcf1cad79d6818be8ad4f9212d8308b
sergenadin <150278010+sergenadin@users.noreply.github.com>
2024-03-08 13:38:16 +0100

    Ajout de la carte Google Maps

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
@@ -13,6 +13,17 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          <div>
+            <h3>Serge Nadin</h3>
+            <p>Ville: Rabat</p>
+            <p>Profession: Web Dev</p>
+            <p>Tel: 0607576502</p>
+            <img src="serge.jpg" alt="Votre photo" style={{ width: '100px' }} />
+          </div>
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
